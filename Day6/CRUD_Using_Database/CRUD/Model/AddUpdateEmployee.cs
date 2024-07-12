3de843cc17a94eeec1f8079d0242a2edb40458a1
Hardik Mahajan <hardikmahajan2010@gmail.com>
2024-07-12 19:40:13 +0530

    Day 6

---
+++ Day6/CRUD_Using_Database/CRUD/Model/AddUpdateEmployee.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD.Model
+{
+    public class AddUpdateEmployee 
+    {
+        public required string FirstName { get; set; }
+        public string? LastName { get; set; } = string.Empty;
+        public Boolean isActive { get; set; } = true;
+    }
+}
---
+++ Day6/CRUD_Using_Database/CRUD/Service/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.Model;
+
+namespace CRUD.Service
+{
+    public interface IEmployeeervice
+    {
+        Task<List<Employee>> GerAllHerosAsync(bool? isActive);
+        Task<Employee ?> GetHeroesById(int id);
+        Task<Employee?> AddEmployee(AddUpdateEmployee obj);
+        Task<Employee?> UpdateEmployee(int id, AddUpdateEmployee obj);
+        Task<bool> DeleteHerosById(int id);
+
+    }
+}
