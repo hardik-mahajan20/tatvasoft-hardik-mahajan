496c1b631774774fd782ebe66221638eeeb66234
Hardik Mahajan <hardikmahajan2010@gmail.com>
2024-07-13 21:29:19 +0530

    Day 7

---
+++ Day 7/Mission_CRUD/Authentication/Authentication/Controllers/MissionController.cs
@@ -0,0 +1,72 @@
+﻿using Authentication.Entities;
+using Authentication.Model;
+using Authentication.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+//THis is api for mission manipulation
+
+namespace Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   // [Authorize]
+    public class MissionController : ControllerBase
+    {
+        private readonly IMission _missionRepository;
+
+        public MissionController(IMission missionRepository)
+        {
+            _missionRepository = missionRepository;
+        }
+
+        [HttpGet("GetMissions")]
+        public async Task<IActionResult> GetMissionWithDetails()
+        {
+            var missionWithDetails = await _missionRepository.GetMissionsWithDetails();
+            return Ok(missionWithDetails);
+        }
+
+        [HttpGet("GetMissionById/{MissionId}")]
+        public async Task<IActionResult> GetMissionWithDetailsById(int MissionId)
+        {
+            var missionWithDetails = await _missionRepository.GetMissionDetailsById(MissionId);
+            return Ok(missionWithDetails);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMission(int id)
+        {
+            var result = await _missionRepository.DeleteMission(id);
+
+            if (result == "Mission not found")
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] MissionDto mission)
+        {
+            var result = await _missionRepository.AddAndUpdateMission(mission);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] MissionDto mission)
+        {
+          //  if (id != mission.MissionId)
+          //  {   //validation
+          //      return BadRequest("Mission ID in the URL and body don't match");
+          //  }
+
+            var result = await _missionRepository.AddAndUpdateMission(mission);
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ Day 7/Mission_CRUD/Authentication/Authentication/Migrations/20240713135301_Initial.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Authentication.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Missions",
+                columns: table => new
+                {
+                    MissionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Introduction = table.Column<string>(type: "text", nullable: true),
+                    Challenge = table.Column<string>(type: "text", nullable: true),
+                    TotalSeats = table.Column<int>(type: "integer", nullable: true),
+                    SeatsLeft = table.Column<int>(type: "integer", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Deadline = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ThemeId = table.Column<int>(type: "integer", nullable: true),
+                    OrganizationId = table.Column<int>(type: "integer", nullable: true),
+                    CityId = table.Column<int>(type: "integer", nullable: true),
+                    CountryId = table.Column<int>(type: "integer", nullable: true),
+                    MissionImage = table.Column<string>(type: "text", nullable: true),
+                    MissionType = table.Column<int>(type: "integer", nullable: true),
+                    MissionObject = table.Column<string>(type: "text", nullable: true),
+                    MissionAchieved = table.Column<int>(type: "integer", nullable: true),
+                    Availability = table.Column<int>(type: "integer", nullable: true),
+                    ImageURL = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Missions", x => x.MissionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Themes",
+                columns: table => new
+                {
+                    ThemeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ThemeName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Themes", x => x.ThemeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Missions");
+
+            migrationBuilder.DropTable(
+                name: "Themes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
